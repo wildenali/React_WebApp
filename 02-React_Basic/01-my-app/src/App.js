e412ffcff9513777b105fa1ba3cfa5471fadbd32
wildenali <wildenwildenaliali@gmail.com>
2020-07-27 10:26:58 +0700

    pengenalan dasar app pada react

--- 02-React_Basic/01-my-app/src/App.js
+++ 02-React_Basic/01-my-app/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const namaDepan = "wilden";
+    return (
+      <div>
+        <h1 className="main-title">Welcome to .................{ namaDepan } dan { 23 + 45 }</h1>
+        <p>Hay semuanya</p>
+      </div>
+    );
+  }
 }
 
 export default App;
