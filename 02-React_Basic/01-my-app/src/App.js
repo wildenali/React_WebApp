382026d5b3d5aa9168a33dfb8cf0e7226c4fcb81
wildenali <wildenwildenaliali@gmail.com>
2020-07-27 10:49:42 +0700

    Membuat Functional Componen

--- 02-React_Basic/01-my-app/src/App.js
+++ 02-React_Basic/01-my-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MenuMakanan from './components/MenuMakanan/MenuMakanan';
 import './App.css';
 
 class App extends Component {
@@ -7,10 +8,7 @@ class App extends Component {
       <div className="App">
         <h1>Caffee Wilden Ali</h1>
           <h2>Menu Makanan</h2>
-          <div style={{ border: "1px solid black", width: 300, margin: 'auto'}}>
-            <p>Nama Menu: Indomie Origina</p>
-            <p>Harga: Rp. 35.000</p>
-          </div>
+          <MenuMakanan />
           
           <h2>Menu Minumam</h2>
           <div style={{ border: "1px solid black", width: 300, margin: 'auto'}}>
---
+++ 02-React_Basic/01-my-app/src/components/MenuMakanan/MenuMakanan.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// nama function diawali lowercase
+var menuMakanan = () => {
+  return(
+    <div style={{ border: "1px solid black", width: 300, margin: 'auto'}}>
+      <p>Nama Menu: Indomie Origina</p>
+      <p>Harga: Rp. 35.000</p>
+    </div>
+  )
+}
+
+export default menuMakanan;
