0c6f01fb39e61608536dc4a389a11d30181d989d
wildenali <wildenwildenaliali@gmail.com>
2020-07-27 13:05:44 +0700

    Events Handling, misal kaya button di pencet, ada yg berubah

--- 02-React_Basic/01-my-app/src/App.js
+++ 02-React_Basic/01-my-app/src/App.js
@@ -13,17 +13,24 @@ class App extends Component {
     }
   }
 
+  handleGantiNama = () => {
+    console.log('button di pencet');
+    this.setState({ namaCafe: 'Robot' })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Caffee { this.state.namaCafe }</h1>
-          <h2>Menu Makanan</h2>
-          <MenuMakanan namaMenu={"Ayam Bakar"} hargaMenu={35000}/>
-          <MenuMakanan namaMenu={"Indomie Rebus"} hargaMenu={15000}/>
-          
-          <h2>Menu Minumam</h2>
-          <MenuMinuman namaMenu={"Es Teh Manis"} hargaMenu={5000}/>
-          <MenuMinuman namaMenu={"Jeruk Panas"} hargaMenu={10000}/>
+        <button onClick={this.handleGantiNama}>Ganti Nama Kafe</button>
+
+        <h2>Menu Makanan</h2>
+        <MenuMakanan namaMenu={"Ayam Bakar"} hargaMenu={35000}/>
+        <MenuMakanan namaMenu={"Indomie Rebus"} hargaMenu={15000}/>
+        
+        <h2>Menu Minumam</h2>
+        <MenuMinuman namaMenu={"Es Teh Manis"} hargaMenu={5000}/>
+        <MenuMinuman namaMenu={"Jeruk Panas"} hargaMenu={10000}/>
       </div>
     );
   }
