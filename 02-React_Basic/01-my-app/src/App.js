ed99a299fec96da7abaf61067e2862a0d40f498d
wildenali <wildenwildenaliali@gmail.com>
2020-07-27 10:44:08 +0700

    Membuat React Element di App.js

--- 02-React_Basic/01-my-app/src/App.js
+++ 02-React_Basic/01-my-app/src/App.js
@@ -3,11 +3,20 @@ import './App.css';
 
 class App extends Component {
   render() {
-    const namaDepan = "wilden";
     return (
-      <div>
-        <h1 className="main-title">Welcome to .................{ namaDepan } dan { 23 + 45 }</h1>
-        <p>Hay semuanya</p>
+      <div className="App">
+        <h1>Caffee Wilden Ali</h1>
+          <h2>Menu Makanan</h2>
+          <div style={{ border: "1px solid black", width: 300, margin: 'auto'}}>
+            <p>Nama Menu: Indomie Origina</p>
+            <p>Harga: Rp. 35.000</p>
+          </div>
+          
+          <h2>Menu Minumam</h2>
+          <div style={{ border: "1px solid black", width: 300, margin: 'auto'}}>
+            <p>Nama Menu: Kopi Late</p>
+            <p>Harga: Rp. 25.000</p>
+          </div>
       </div>
     );
   }
