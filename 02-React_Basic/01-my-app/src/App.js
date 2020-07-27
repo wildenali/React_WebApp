9fd5918cd1dc42f34e3f8e6b0ceb1b66bec14211
wildenali <wildenwildenaliali@gmail.com>
2020-07-27 17:51:43 +0700

    tambah handleChangeNamaPembeli, handleChangeKeterangan, handleChangeJenisPembayaran

--- 02-React_Basic/01-my-app/src/App.js
+++ 02-React_Basic/01-my-app/src/App.js
@@ -20,13 +20,28 @@ class App extends Component {
         { id: 3, nama: 'Jeruk Dingin', harga: 41000 },
         { id: 4, nama: 'Jus Tomat', harga: 51000 },
         { id: 5, nama: 'Kopi Pahit', harga: 11000 },
-      ]
+      ],
+      namaPembeli: '',
+      keterangan: '',
+      jenisPembarayan: 'cash',
     }
   }
 
   handleGantiNama = () => {
     console.log('button di pencet');
     this.setState({ namaCafe: 'Robot' })
+  }
+
+  handleChangeNamaPembeli = (event) => {
+    this.setState({ namaPembeli: event.target.value })
+  }
+  
+  handleChangeKeterangan = (event) => {
+    this.setState({ keterangan: event.target.value })
+  }
+  
+  handleChangeJenisPembayaran = (event) => {
+    this.setState({ jenisPembarayan: event.target.value })
   }
 
   render() {
@@ -42,6 +57,46 @@ class App extends Component {
       <div className="App">
         <h1>Caffee { this.state.namaCafe }</h1>
         <button className="btn btn-success" onClick={this.handleGantiNama}>Ganti Nama Kafe</button>
+
+        <div className="row" style={{ marginTop: '10px' }}>
+          <div className="col-md-4 offset-md-4">
+            <div className="form-group">
+              <label>Nama Pembeli:</label>
+              <input
+                className="form-control"
+                name="namaPembeli"
+                onChange={this.handleChangeNamaPembeli}
+                value={this.state.namaPembeli}
+              />
+              <small>Nama Pembeli: {this.state.namaPembeli}</small>
+              <br/>
+
+              <label>Keterangan:</label>
+              <textarea
+                className="form-control"
+                name="keterangan"
+                onChange={this.handleChangeKeterangan}
+                value={this.state.keterangan}
+              />
+              <small>Keterangan: {this.state.keterangan}</small>
+              <br/>
+
+              <label>Jenis Pembayaran:</label>
+              <select
+                className="form-control"
+                name="jenisPembayaran"
+                value={this.state.value}
+                onChange={this.handleChangeJenisPembayaran}
+              >
+                <option value="cash">Cash</option>
+                <option value="kartuKredit">Kartu Kredit</option>
+                <option value="gopay">Gopay</option>
+              </select>
+              <small>Jenis Pembayaran: {this.state.jenisPembarayan}</small>
+
+            </div>
+          </div>
+        </div>
 
         <h2 className="header-menu">Menu Makanan</h2>
         <MenuMakanan 
