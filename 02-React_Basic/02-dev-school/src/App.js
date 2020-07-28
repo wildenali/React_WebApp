36461450dad78dd05e984aa2fa1874caff7c8ecf
wildenali <wildenwildenaliali@gmail.com>
2020-07-28 09:38:27 +0700

    simple in bagian card pakai list

--- 02-React_Basic/02-dev-school/src/App.js
+++ 02-React_Basic/02-dev-school/src/App.js
@@ -22,57 +22,23 @@ class App extends Component {
         <div className="row">
           <div className="col-md-6" style={{ border: '1px solid black' }}>
             <h2>Member</h2>
-            <div className="row pt-2 pb-4">
+            <div className="row">
               
-              <div className="col-md-6">
-                <div className="card" style={{ margin: 10 }}>
-                  <div className="card-body">
-                    <h5 className="card-title">ID Member</h5>
-                    <h5 className="card-text">First Name</h5>
-                    <h5 className="card-text">Last Name</h5>
-                    <button className="btn btn-primary">Edit</button>
-                    <button className="btn btn-danger">Delete</button>
+              { this.state.members.map((member, index) => (
+                <div className="col-md-6" key={index}>
+                  <div className="card" style={{ margin: 10 }}>
+                    <div className="card-body">
+                      <h5 className="card-title">ID Member</h5>
+                      <h5 className="card-text">First Name</h5>
+                      <h5 className="card-text">Last Name</h5>
+                      <button className="btn btn-primary">Edit</button>
+                      <button className="btn btn-danger">Delete</button>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-md-6">
-                <div className="card" style={{ margin: 10 }}>
-                  <div className="card-body">
-                    <h5 className="card-title">ID Member</h5>
-                    <h5 className="card-text">First Name</h5>
-                    <h5 className="card-text">Last Name</h5>
-                    <button className="btn btn-primary">Edit</button>
-                    <button className="btn btn-danger">Delete</button>
-                  </div>
-                </div>
-              </div>
+              )) }
+              
             </div>
-            
-            <div className="row pb-4">
-              <div className="col-md-6">
-                <div className="card" style={{ margin: 10 }}>
-                  <div className="card-body">
-                    <h5 className="card-title">ID Member</h5>
-                    <h5 className="card-text">First Name</h5>
-                    <h5 className="card-text">Last Name</h5>
-                    <button className="btn btn-primary">Edit</button>
-                    <button className="btn btn-danger">Delete</button>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="card" style={{ margin: 10 }}>
-                  <div className="card-body">
-                    <h5 className="card-title">ID Member</h5>
-                    <h5 className="card-text">First Name</h5>
-                    <h5 className="card-text">Last Name</h5>
-                    <button className="btn btn-primary">Edit</button>
-                    <button className="btn btn-danger">Delete</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-
           </div>
           <div className="col-md-6" style={{ border: '1px solid black', }}>
             <h2>Form</h2>
