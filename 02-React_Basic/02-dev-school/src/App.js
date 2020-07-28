107395f93c49b3c70af2a94d12a8ecdf55231273
wildenali <wildenwildenaliali@gmail.com>
2020-07-28 08:58:17 +0700

    first commit for React with Server

---
+++ 02-React_Basic/02-dev-school/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Cek</h1>
+      <button className="btn btn-primary">Tombol</button>
+    </div>
+  );
+}
+
+export default App;
