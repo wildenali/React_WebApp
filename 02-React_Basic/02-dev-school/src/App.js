7fd262b4d28abe1bbd73d081e5a38bd1d674c405
wildenali <wildenwildenaliali@gmail.com>
2020-07-28 10:21:10 +0700

    tambah componenDidMount, untuk get data dari server dengan axios.get

--- 02-React_Basic/02-dev-school/src/App.js
+++ 02-React_Basic/02-dev-school/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
@@ -6,13 +7,19 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      members: [
-        {},
-        {},
-        {},
-        {}
-      ]
+      members: []
     }
+  }
+
+  componentDidMount() {
+    axios.get('https://reqres.in/api/users?page=1')
+      .then(response => {
+        console.log('response', response);
+        this.setState({ members: response.data.data })
+      })
+      .catch(error => {
+        console.log('error', error);
+      })
   }
 
   render() {
@@ -25,12 +32,12 @@ class App extends Component {
             <div className="row">
               
               { this.state.members.map((member, index) => (
-                <div className="col-md-6" key={index}>
+                <div className="col-md-6" key={member.id}>
                   <div className="card" style={{ margin: 10 }}>
                     <div className="card-body">
-                      <h5 className="card-title">ID Member</h5>
-                      <h5 className="card-text">First Name</h5>
-                      <h5 className="card-text">Last Name</h5>
+                      <h5 className="card-title">ID: {member.id}</h5>
+                      <h5 className="card-text">First Name: {member.first_name}</h5>
+                      <h5 className="card-text">Last Name: {member.last_name}</h5>
                       <button className="btn btn-primary">Edit</button>
                       <button className="btn btn-danger">Delete</button>
                     </div>
