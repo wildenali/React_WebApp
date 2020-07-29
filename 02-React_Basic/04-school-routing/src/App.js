ce747458e52cca4aeda20dfdb89b5530c0d72b49
wildenali <wildenwildenaliali@gmail.com>
2020-07-29 14:47:58 +0700

    install react-router-dom

--- 02-React_Basic/04-school-routing/src/App.js
+++ 02-React_Basic/04-school-routing/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hay</h1>
-        <button className="btn btn-primary">OK</button>
-      </div>
+      <Router>
+        <div className="App">
+          <h1>Hay</h1>
+          <button className="btn btn-primary">OK</button>
+        </div>
+      </Router>
     );
   }
 }
