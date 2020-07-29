396f599e2b99e400ab9d4d0d9173f3e55d12de8a
wildenali <wildenwildenaliali@gmail.com>
2020-07-29 15:06:49 +0700

    Routing sederhana dulu

--- 02-React_Basic/04-school-routing/src/App.js
+++ 02-React_Basic/04-school-routing/src/App.js
@@ -8,8 +8,22 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <h1>Hay</h1>
-          <button className="btn btn-primary">OK</button>
+          <nav className="navbar navbar-expand-sm navbar-light bg-light">
+            <Link className="navbar-brand" href="/">Negara Api Course</Link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <Link className="nav-item nav-link" to="/">Home</Link>
+                <Link className="nav-item nav-link" to="/premium">Premium</Link>
+                <Link className="nav-item nav-link" to="/fireschool">Fire School</Link>
+              </div>
+            </div>
+          </nav>
+          <Route path="/" exact component={() => <h4>Home</h4>}></Route>
+          <Route path="/premium" component={() => <h4>Premium</h4>}></Route>
+          <Route path="/fireschool" component={() => <h4>FireSchool</h4>}></Route>
         </div>
       </Router>
     );
