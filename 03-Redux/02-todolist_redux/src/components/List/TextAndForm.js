1825eac9f875b359de07c767dcfc793c67169825
wildenali <wildenwildenaliali@gmail.com>
2020-07-30 11:11:32 +0700

    Edit state, persiapkan file dan code untuk Edit list

---
+++ 03-Redux/02-todolist_redux/src/components/List/TextAndForm.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+
+class TextAndForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      viewMode: 'show',
+      text: props.todo
+    }
+  }
+
+  // fungsi untuk hadnle ketika user klik tombol edit
+  editButtonHandler = () => {
+    this.setState({ viewMode: 'edit' })
+  }
+  // fungsi untuk hadnle ketika user klik tombol save
+  saveButtonHandler = () => {
+    
+  }
+  // fungsi untuk hadnle ketika user klik tombol cancel
+  cancelButtonHandler = () => {
+    this.setState({ viewMode: 'show' })
+  }
+  // fungsi untuk hadnle ketika user klik tombol delete
+  deleteButtonHandler = () => {
+    this.props.onDelete()
+  }
+
+  // fungsi untuk handle ketika user edit text di form
+  onChangeHandler = (e) => {
+    this.setState({ text: e.target.value })
+  }
+
+
+  render() {
+
+    // default tampilan todo berupa text
+    var todo = (
+      <p>{this.state.todo}</p>
+    )
+
+    // default tampilan disamping text todo berupa tombol edit
+    var buttonEdit = (
+      <button
+        className="btn btn-primary"
+        onClick={this.editButtonHandler}
+      >
+        Edit
+      </button>
+    )
+
+    // Jika viewMode baruba menjadi 'edit'
+    if (this.state.viewMode === 'edit') {
+      // ganti element text todo menjadi form input
+      todo = (
+        <input
+          className="form-control"
+          type="text"
+          value={this.state.todo}
+          onChange={this.onChangeHandler}
+        />
+      )
+
+      // ganti tombol edit menjadi tombol save dan cancel
+      buttonEdit = (
+        <>
+          <button className="btn btn-success" onClick={this.saveButtonHandler}>Save</button>
+          <button className="btn btn-warning" onClick={this.cancelButtonHandler}>Cancel</button>
+        </>
+      )
+    }
+
+    return (
+      <div className="row">
+        <div className="col-8">{todo}</div>
+        <div className="col-4">
+          {buttonEdit}
+          <button className="btn btn-danger" onClick={this.deleteButtonHandler}>Delete</button>
+        </div>
+      </div>
+    )
+  }
+}
