0e755ab56b5589731a1706e609194662b3fd259a
wildenali <wildenwildenaliali@gmail.com>
2020-07-30 10:43:51 +0700

    Delete state, pakai metho filter untuk delete List

--- 03-Redux/02-todolist_redux/src/store/index.js
+++ 03-Redux/02-todolist_redux/src/store/index.js
@@ -11,6 +11,28 @@ const rootReducer = (state = initialState, action) => {
         ...state,
         todos: [...state.todos, action.payload]
       }
+    case 'DELETE_TODO':
+      // ---------------- Cara 1 ----------------
+      // // menghapus dengan method splice
+      // // copu state todos
+      // let newTodos = [...state.todos]
+      // // hapus element array
+      // newTodos.splice(action.payload.index, 1)
+      // // return state baru
+      // return {
+      //   ...state,
+      //   todos: newTodos
+      // }
+      // ---------------- Cara 1 ----------------
+
+      // ---------------- Cara 2 ----------------
+      // Menghapus dengan methos filter
+      let newTodos = state.todos.filter((element, index) => index !== action.payload)
+      return {
+        ...state,
+        todos: newTodos
+      }
+      // ---------------- Cara 2 ----------------
     default:
       return state
   }
