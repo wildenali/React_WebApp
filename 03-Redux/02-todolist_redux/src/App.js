62d123a032e2b5533cfee55c51a3062144dc9891
wildenali <wildenwildenaliali@gmail.com>
2020-07-30 09:20:51 +0700

    redux project todolist first commit

---
+++ 03-Redux/02-todolist_redux/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Cek</h1>
+        <button className="btn btn-primary">Ok</button>
+      </div>
+    );
+  }
+}
+
+export default App;
