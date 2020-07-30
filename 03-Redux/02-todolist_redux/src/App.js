8bbe2140cbd9258700183e1fc281188dc054d5ab
wildenali <wildenwildenaliali@gmail.com>
2020-07-30 09:53:21 +0700

    rapihin file App.js yg ketinggalan

--- 03-Redux/02-todolist_redux/src/App.js
+++ 03-Redux/02-todolist_redux/src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import Form from './components/Form/Form'
+import List from './components/List/List'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      todos: []
+    }
+  }
+
+  handleSubmit = (todo) => {
+    this.setState(prevState => (
+      {todos: prevState.todos.concat(todo) }
+    ))
+  }
+  
   render() {
     return (
-      <div className="App">
-        <h1>Cek</h1>
-        <button className="btn btn-primary">Ok</button>
+      <div className="App container">
+        <Form submit={this.handleSubmit}/>
+        <List todos={this.state.todos}/>
       </div>
     );
   }
